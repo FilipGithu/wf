0495befd6fa80a19cbbc25a3998a5ee664d4c20d
filip <filipgaman@gmail.com>
2019-03-27 10:02:23 +0100

    init compsoer

---
+++ OOP/Exercice3/NotAllowedRoleException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 3/26/2019
+ * Time: 1:03 PM
+ */
+
+namespace Exception;
+
+
+class NotAllowedRoleException
+{
+
+}
---
+++ OOP/Exercice3/src/autoload.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 3/26/2019
+ * Time: 12:37 PM
+ */
